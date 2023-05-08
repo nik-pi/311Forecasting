aca11d7c757f2773e50be0ca9f9705721b8b74b4
nik <niklas.piepenbreier@gmail.com>
2023-05-08 08:39:02 -0400

    Created update_data function

--- model/getdata.py
+++ model/getdata.py
@@ -6,33 +6,43 @@ import os
 load_dotenv()
 APP_TOKEN = os.getenv('APP_TOKEN')
 
-def get_max_date() -> str:
+def _get_max_date() -> str:
     df = pd.read_csv('model/vals.csv')
     date_str = df.iloc[-1, 0]
     date_str += 'T00:00:00.000'
     return date_str
 
-def construct_query() -> str:
+def _construct_query() -> str:
     url = ' https://data.cityofnewyork.us/resource/erm2-nwe9.json'
     query_app_token = f'?$$app_token={APP_TOKEN}'
     query_select = '&$SELECT=date_trunc_ymd(created_date) as Date, count(*) as Num&$GROUP=date_trunc_ymd(created_date)'
-    query_where = f'&$WHERE=created_date >= "{get_max_date()}"'
+    query_where = f'&$WHERE=created_date >= "{_get_max_date()}"'
     query_order = '&$ORDER=date_trunc_ymd(created_date)'
     query = f'{url}{query_app_token}{query_select}{query_where}{query_order}'
     
     return query
 
-def filter_bad_data(
+def _filter_bad_data(
         data: list[dict]
         ) -> list[dict]:
     
-    if int(data[-1]['Num']) < 1_000:
+    if int(data[-1]['Num']) < 3_000:
         data.pop()
 
     return data
 
+def update_data() -> None:
+    query = _construct_query()
+    resp = requests.get(query).json()
+    resp = _filter_bad_data(resp)
+    df = pd.DataFrame(resp)
+    df['Date'] = pd.to_datetime(df['Date'])
+    df['Date'] = df['Date'].dt.strftime('%Y-%m-%d')
+    old = pd.read_csv('model/vals.csv')
+    df = pd.concat([old, df], ignore_index=True)
+    df = df.drop_duplicates(subset=['Date'], keep='first')
+    df.to_csv('model/vals.csv', index=False)
 
-query = construct_query()
-resp = requests.get(query).json()
-resp = filter_bad_data(resp)
-print(resp)
+
+if __name__ == "__main__":
+    update_data()
