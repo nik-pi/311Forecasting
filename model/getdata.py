c8a88e905269c44cc9de3635d15c2649c38c2d64
nik <niklas.piepenbreier@gmail.com>
2023-05-10 19:15:41 -0400

    updated query

--- model/getdata.py
+++ model/getdata.py
@@ -13,9 +13,10 @@ def _get_max_date() -> str:
     return date_str
 
 def _construct_query() -> str:
-    url = ' https://data.cityofnewyork.us/resource/erm2-nwe9.json'
+    # url = 'https://data.cityofnewyork.us/resource/erm2-nwe9.json?$where=created_date>"2023-05-07"'
+    url = 'https://data.cityofnewyork.us/resource/erm2-nwe9.json'
     query_app_token = f'?$$app_token={APP_TOKEN}'
-    query_select = '&$SELECT=date_trunc_ymd(created_date) as Date, count(*) as Num&$GROUP=date_trunc_ymd(created_date)'
+    query_select = '?&$SELECT=date_trunc_ymd(created_date) as Date, count(*) as Num&$GROUP=date_trunc_ymd(created_date)'
     query_where = f'&$WHERE=created_date >= "{_get_max_date()}"'
     query_order = '&$ORDER=date_trunc_ymd(created_date)'
     query = f'{url}{query_select}{query_where}{query_order}'
@@ -25,22 +26,22 @@ def _construct_query() -> str:
 def _filter_bad_data(
         data: list[dict]
         ) -> list[dict]:
-    if int(data[-1]['Num']) < 3_000:
-        print(data)
+    last_record = data[-1].get('Num')
+    last_record = int(last_record)
+    if last_record < 3_000:
         data.pop()
 
     return data
 
 def update_data() -> None:
     query = _construct_query()
-    resp = requests.get(query).text
-    with open('log.txt', 'a') as file:
-        file.write(resp)
-    # if resp:
-    #     resp = _filter_bad_data(resp)
-    # df = pd.DataFrame(resp)
-    # df['Date'] = pd.to_datetime(df['Date'])
-    # df['Date'] = df['Date'].dt.strftime('%Y-%m-%d')
+    resp = requests.get(query).json()
+    if resp:
+        resp = _filter_bad_data(resp)
+    df = pd.DataFrame(resp)
+    df['Date'] = pd.to_datetime(df['Date'])
+    df['Date'] = df['Date'].dt.strftime('%Y-%m-%d')
+    df.to_csv('log.txt')
     # old = pd.read_csv('model/vals.csv')
     # df = pd.concat([old, df], ignore_index=True)
     # df = df.drop_duplicates(subset=['Date'], keep='first')
